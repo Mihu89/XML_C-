d0f2431d296961ad4731b7f543440b7f17e0338d
Mihail <mihail_coscodan@mail.ru>
2018-10-27 11:14:06 +0300

    Create Xml file

---
+++ ConsoleXml/ConsoleXml/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ConsoleXml {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("XML");
+            CreateXmlFile();
+        }
+
+        public static void CreateXmlFile() {
+            //create list of users
+            var listUsers = new List<User>() {
+                new User{ Name="John Rambo",Age=25,Company="Holywood"},
+                new User{ Name="John Smith",Age=35,Company="LuterCorp"},
+                new User{ Name="Jenifer Lopez",Age=22,Company="Muzic"},
+            };
+            XmlSerializer serialiser = new XmlSerializer(typeof(List<User>));
+            TextWriter Filestream = new StreamWriter(@"D:\Users.xml");
+            serialiser.Serialize(Filestream, listUsers);
+            Filestream.Close();
+            
+        }
+    }
+}
---
+++ ConsoleXml/ConsoleXml/User.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ConsoleXml {
+    public class User {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+    }
+}
